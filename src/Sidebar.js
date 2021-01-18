0e91baeb0f77163fe051ec73d3118bac0e520783
Sailesh-2209 <saileshr2209@gmail.com>
2021-01-18 23:50:47 +0530

    added dropdown to select sorting algorithm

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,7 +3,19 @@ import React from 'react';
 function Sidebar() {
     return(
         <div className="sidebar">
-
+            <form className="form">
+                <label className='form-label' htmlFor="algorithm">
+                    Choose an Algorithm
+                </label>
+                <br/>
+                <select className='form-select' name="algortithm" id="algorithm">
+                    <option className='form-option' value="bubble">Bubble Sort</option>
+                    <option className='form-option' value="selection">Selection Sort</option>
+                    <option className='form-option' value="insertion">Insertion Sort</option>
+                    <option className='form-option' value="quick">Quick Sort</option>
+                    <option className='form-option' value="merge">Merge Sort</option>
+                </select>
+            </form>
         </div>
     );
 }
