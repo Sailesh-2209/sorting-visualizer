823954b4b42c23dd8a94fe6d7d9d04a11b9a2f71
Sailesh-2209 <saileshr2209@gmail.com>
2021-01-23 20:40:57 +0530

    impolemented quick sort!

--- src/algorithms/quick.js
+++ src/algorithms/quick.js
@@ -9,7 +9,7 @@ const quickSort = (arr, ele) => {
         return new Promise(resolve => setTimeout(resolve, milliseconds));
     }
 
-    const partition = (array, low, high) => {
+    const partition = async (array, low, high) => {
         let i = (low - 1);
         let pivot = array[high];
         for (let j = low; j < high + 1; j++) {
@@ -20,17 +20,28 @@ const quickSort = (arr, ele) => {
                 ele.current.childNodes[i].style.backgroundColor = 'white';
                 ele.current.childNodes[j].style.backgroundColor = 'white';
 
+                await sleep(40);
+
                 ele.current.childNodes[i].style.backgroundColor = '#2177f8';
                 ele.current.childNodes[j].style.backgroundColor = '#2177f8';
             }
         }
         [ array[i + 1], array[high] ] = [ array[high], array[i + 1] ]; 
+        ele.current.childNodes[i + 1].style.backgroundColor = 'white';
+        ele.current.childNodes[high].style.backgroundColor = 'white';
+        [ ele.current.childNodes[i + 1].style.height, ele.current.childNodes[high].style.height ] = [ ele.current.childNodes[high].style.height, ele.current.childNodes[i + 1].style.height ];
+
+        await sleep(40);
+
+        ele.current.childNodes[i + 1].style.backgroundColor = '#2177f8';
+        ele.current.childNodes[high].style.backgroundColor = '#2177f8';
+
         return (i + 1);
     }
 
-    const main = (arr, low, high) => {
+    const main = async (arr, low, high) => {
         if (low < high) {
-            pi = partition(arr, low, high);
+            pi = await partition(arr, low, high);
             main(arr, low, pi - 1);
             main(arr, pi + 1, high);
         }
